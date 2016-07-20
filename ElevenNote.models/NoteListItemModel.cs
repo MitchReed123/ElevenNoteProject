c6fa6a090b1545a7c8358bcefdeabf1afd82e52b
MitchReed123 <reed.mitch@live.com>
2016-07-20 11:54:12 -0400

    Last commit before lunch

---
+++ ElevenNote.models/NoteListItemModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.models
+{
+    public class NoteListItemModel
+    {
+
+        public int NoteId { get; set; }
+
+        public string Title { get; set; }
+
+        public DateTimeOffset CreatedUtc { get; set; }
+
+        public override string ToString()
+        {
+            return $"[{NoteId}] {Title}";
+        }
+
+    }
+}
