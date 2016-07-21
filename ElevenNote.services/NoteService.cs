0f15b64e68a526f1c6b61abbabd42d9ab7972909
MitchReed123 <reed.mitch@live.com>
2016-07-21 10:38:44 -0400

    Changes to NoteService.cs,NotesController.cs, and Index.cshtml

---
+++ ElevenNote.services/NoteService.cs
@@ -0,0 +1,41 @@
+﻿using ElevenNote.Data;
+using ElevenNote.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.services
+{
+    public class NoteService
+    {
+        private readonly Guid _userId;
+
+        public NoteService(Guid userId)
+        {
+            _userId = userId;
+        }
+        public IEnumerable<NoteListItemModel> GetNotes()
+        {
+            using (var ctx = new ElevenNoteDbContext())
+            {
+                var query =
+                    ctx
+                        .Notes
+                        .Where(e => e.OwnerId == _userId)
+                        .Select(
+                             e =>
+                                new NoteListItemModel
+                                {
+                                    NoteId = e.NoteId,
+                                    Title = e.Title,
+                                    CreatedUtc = e.CreatedUtc
+                                }
+                        );
+
+                return query.ToArray();
+            }
+        }
+    }
+}
---
+++ ElevenNote.web/Controllers/NotesController.cs
@@ -0,0 +1,24 @@
+﻿using ElevenNote.services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ElevenNote.web.Controllers
+{
+    [Authorize]
+    public class NotesController : Controller
+    {
+        // GET: Notes
+        public ActionResult Index()
+        {
+            var notes =
+                new NoteService()
+                .GetNotes();
+                
+
+            return View(notes);
+        }
+    }
+}
